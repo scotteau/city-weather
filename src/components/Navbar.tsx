206684f546d9f4c8761c8a236abb8c7642057eeb
Scott Wang <scotteau@gmail.com>
2020-04-27 15:50:34 +0800

    refactored navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,39 @@
 import React from "react";
-import logo from "../logo.svg";
-import logoDark from "../logo-dark.svg";
+import logo from "../assets/logo.svg";
+import logoDark from "../assets/logo-dark.svg";
 import { Mode, NavAction } from "../Model";
 
 interface navProps {
-  theme?: Mode;
+  theme: Mode;
   onChanges: any;
   editMode: boolean;
 }
 const Navbar = ({ theme = Mode.light, onChanges, editMode }: navProps) => {
   const currentLogo = theme === Mode.light ? logo : logoDark;
 
+  const smartTextColor = theme === Mode.dark ? { color: "white" } : {};
+  const smartBgColor = theme === Mode.dark ? { backgroundColor: "#212121" } : {};
+  const hidden = { display: "none" };
+  const addingCity = () => !editMode && onChanges(NavAction.ADD_A_CITY);
+  const toggledEditMode = () => !editMode && onChanges(NavAction.TOGGLE_THEME);
+  const renderNavFooter = () => {
+    return (
+      <>
+        powered by{" "}
+        <a
+          href={"https://unsplash.com/"}
+          target={"_blank"}
+          rel={"noopener noreferrer"}
+          style={smartTextColor}
+        >
+          unsplash
+        </a>
+      </>
+    );
+  };
+
   return (
-    <div
-      className="navbar"
-      style={theme === Mode.dark ? { backgroundColor: "#212121" } : {}}
-    >
+    <div className="navbar" style={smartBgColor}>
       <header className={"navbar__logo"}>
         <a href={"/"}>
           <img src={currentLogo} alt={"logo"} />
@@ -23,54 +41,22 @@ const Navbar = ({ theme = Mode.light, onChanges, editMode }: navProps) => {
       </header>
 
       <ul className={"navbar__links"}>
-        <li
-          onClick={() => {
-            if (!editMode) {
-              onChanges(NavAction.ADD_A_CITY);
-            }
-          }}
-          style={editMode ? { display: "none" } : {}}
-        >
-          <span style={theme === Mode.dark ? { color: "white" } : {}}>
-            add a city
-          </span>
+        <li onClick={() => addingCity()} style={editMode ? hidden : {}}>
+          <span style={smartTextColor}>add a city</span>
         </li>
 
-        <li
-          onClick={() => {
-            onChanges(NavAction.TOGGLE_EDIT);
-          }}
-        >
-          <span style={theme === Mode.dark ? { color: "white" } : {}}>
-            {editMode ? "back" : "customize"}
-          </span>
+        <li onClick={() => onChanges(NavAction.TOGGLE_EDIT)}>
+          <span style={smartTextColor}>{editMode ? "back" : "customize"}</span>
         </li>
 
-        <li
-          onClick={() => {
-            if (!editMode) {
-              onChanges(NavAction.TOGGLE_THEME);
-            }
-          }}
-          style={editMode ? { display: "none" } : {}}
-        >
-          <span style={theme === Mode.dark ? { color: "white" } : {}}>
+        <li onClick={() => toggledEditMode()} style={editMode ? hidden : {}}>
+          <span style={smartTextColor}>
             {theme === Mode.dark ? "day" : "night"}
           </span>
         </li>
       </ul>
 
-      <footer style={theme === Mode.dark ? { color: "white" } : {}}>
-        powered by{" "}
-        <a
-          href={"https://unsplash.com/"}
-          target={"_blank"}
-          rel={"noopener noreferrer"}
-          style={theme === Mode.dark ? { color: "white" } : {}}
-        >
-          unsplash
-        </a>
-      </footer>
+      <footer style={smartTextColor}>{renderNavFooter()}</footer>
     </div>
   );
 };
